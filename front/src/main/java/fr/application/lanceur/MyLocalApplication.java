02f1ce1f2bb35f658770535cff4837066da703be
stephan <stephan.parichon.pro@gmail.com>
2021-09-27 14:25:32 +0200

    WI-10 : mise en place du projet front

---
+++ front/src/main/java/fr/application/lanceur/MyLocalApplication.java
@@ -0,0 +1,13 @@
+package fr.application.lanceur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLocalApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(MyLocalApplication.class, args);
+    }
+
+}
